8231bb8f28d340e36aa329033c87e4cfb55ca85b
Manuelherrera22 <albumcoervus123@gmail.com>
2025-09-21 16:05:25 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from '@/App';
 import '@/index.css';
+import '@/styles/modal-fixes.css';
 import { LanguageProvider } from '@/contexts/LanguageContext';
 import { MockAuthProvider } from '@/contexts/MockAuthContext.jsx';
 import { HelmetProvider } from 'react-helmet-async';
